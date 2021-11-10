5bf1236dc967548436589026091c51fe1c44c54d
VoThiNa2010 <na.vo2010@hcmut.edu.vn>
2021-11-10 23:44:23 +0700

    fix error

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -27,10 +27,6 @@ const HomeScreen = ({match}) => {
             src="images/bia1.png"
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -38,11 +34,6 @@ const HomeScreen = ({match}) => {
             src="images/bia4.png"
             alt="Second slide"
           />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -50,13 +41,6 @@ const HomeScreen = ({match}) => {
             src="images/bia3.png"
             alt="Third slide"
           />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
       <h1>Danh sách sản phẩm</h1>
